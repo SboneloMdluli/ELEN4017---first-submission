cb31ebfae6eb761a340bbc9f764be7943f855966
SboneloMdluli <1101772@students.wits.ac.za>
2020-05-09 21:47:09 +0200

    final task 2.3 (1)

--- simpleServer.py
+++ simpleServer.py
@@ -10,41 +10,24 @@ BUFFER_SIZE = 4096
 class FTP_SERVER:
     def __init__(self, conn):
         self.conn = conn
-        self.clntAddr = None
 
     def printClientMessage(self):
         """ print message from client """
-        msg, self.clntAddr = self.conn.recvfrom(BUFFER_SIZE)
+        msg = self.conn.recv(BUFFER_SIZE)
         print("\nRecieved message: {}".format(msg))
 
-    def echo(self):
-        """ echo message from client"""
-        msg, self.clntAddr = self.conn.recvfrom(BUFFER_SIZE)
-
-        try:
-            self.conn.sendto(msg, self.clntAddr)
-            print("\n message echoed")
-
-        except:
-            print("\n message could not be echoed")
-
-    def quit(self):
-        self.conn.close()
-
     def start(self):
-
-        while True:
-            cmd = self.conn.recvfrom(BUFFER_SIZE)[0]
-            print(cmd)
-            if cmd == "QUIT":
-                self.quit()
-                break
-            elif cmd == "ECHO":
-                self.echo()
+        self.printClientMessage()
+        self.conn.close()
 
 
 # main program
-commandSocket = socket.socket(type=socket.SOCK_DGRAM)
+commandSocket = socket.socket()
 commandSocket.bind((TCP_IP, TCP_PORT))
-client = FTP_SERVER(commandSocket)
+commandSocket.listen(1)
+CONN, clientControlAddress = commandSocket.accept()
+
+print("\nConnected to by client address: {}".format(clientControlAddress))  # IP address for client
+
+client = FTP_SERVER(CONN)
 client.start()
