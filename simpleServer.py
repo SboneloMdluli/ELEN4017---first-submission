de9d0e63766bdfe76f46e8889171d3371b76377a
SboneloMdluli <1101772@students.wits.ac.za>
2020-05-10 21:48:41 +0200

    Revert "use 5 threads"
    This reverts commit 190da0bf5f0d73b76f836280befa715af52c07b7.

---
+++ simpleServer.py
@@ -0,0 +1,55 @@
+import socket
+import os
+
+print("\nWelcome to the FTP server.")
+
+TCP_IP = "127.0.1.1"  # local address
+TCP_PORT = 1026
+BUFFER_SIZE = 4096
+
+
+class FTP_SERVER:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def printClientMessage(self):
+        """ print message from client """
+        msg = self.conn.recv(BUFFER_SIZE)
+        print("\nRecieved message: {}".format(msg))
+
+    def echo(self):
+        """ echo message from client"""
+        msg = self.conn.recv(BUFFER_SIZE)
+        try:
+            self.conn.send(msg.encode())
+            print("\n message echoed back to client")
+
+        except:
+            print("\n message could not be echoed")
+
+    def quit(self):
+        self.conn.close()
+
+
+    def start(self):
+
+        while True:
+            cmd = self.conn.recv(BUFFER_SIZE)
+            print(cmd)
+            if cmd == "QUIT":
+                self.quit()
+                break
+            elif cmd == "ECHO":
+                self.echo()
+
+
+# main program
+commandSocket = socket.socket()
+commandSocket.bind((TCP_IP, TCP_PORT))
+commandSocket.listen(1)
+CONN, clientControlAddress = commandSocket.accept()
+
+print("\nConnected to by client address: {}".format(clientControlAddress))  # IP address for client
+
+client = FTP_SERVER(CONN)
+client.start()
