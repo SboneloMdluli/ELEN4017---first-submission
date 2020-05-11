d9b1d47ce3f4e77200168c96377ca9fc863d1de7
SboneloMdluli <1101772@students.wits.ac.za>
2020-05-12 00:44:23 +0200

    UDP multithreaded

---
+++ Socket Programming with Multithreading/threaded_client.py
@@ -0,0 +1,49 @@
+import socket
+
+print("\nWelcome to the FTP client.")
+
+SERVER_TCP_IP = "127.0.1.1"  # local address
+SERVER_TCP_PORT = 11002
+BUFFER_SIZE = 4096
+
+class FTP_CLIENT:
+
+    def __init__(self, conn, SERVER_UDPADDR):
+        self.conn = conn
+        self.SERVER_UDPADDR = SERVER_UDPADDR
+
+    def echomsg(self, msg):
+        """ECHO message to server"""
+
+        try:
+            self.conn.sendto(str.encode(msg), SERVER_UDPADDR)
+            print("\nmessage sent")
+            svrmag = self.conn.recvfrom(BUFFER_SIZE)[0]
+            print("\nechoed message from the server: {}".format(svrmag))
+            print("\n-----------------------------------------")
+        except:
+            print("\n message could not be sent")
+
+    def quit(self):
+        self.conn.close()
+        print("Stop connection")
+
+
+    def start(self):
+        while True:
+            # Listen for a command
+            msg = raw_input("\nEnter a message: ")
+
+            if msg == "QUIT":
+                self.quit()
+                break
+            else:
+                self.echomsg(msg)
+
+
+# main program
+clientSocket = socket.socket(type=socket.SOCK_DGRAM)
+SERVER_UDPADDR = (SERVER_TCP_IP,SERVER_TCP_PORT)
+print("\nConnected to by sever: {}".format(SERVER_UDPADDR))  # IP address for client
+serverInterface = FTP_CLIENT(clientSocket,SERVER_UDPADDR)
+serverInterface.start()
---
+++ Socket Programming with Multithreading/threaded_server.py
@@ -0,0 +1,32 @@
+import socket
+
+TCP_PORT = 11002
+BUFFER_SIZE = 4096
+
+serverSocket = socket.socket(type=socket.SOCK_DGRAM)
+serverSocket.bind(('', TCP_PORT))
+print("The server is ready to receive")
+
+import threading
+class myThread (threading.Thread):
+    def __init__(self, svrmsg, address):
+        threading.Thread.__init__(self)
+        self.address = address
+        self.sentence = svrmsg
+    def run(self):
+        echo(self.sentence, self.address)
+        print("********************")
+
+def echo(msg, clntAddr):
+
+    try:
+        print("\nmessage echoed")
+        serverSocket.sendto(msg.encode(), clntAddr)
+
+    except:
+        print("\nmessage couldnt be echoed")
+
+while True:
+    svrmsg, clntAddr = serverSocket.recvfrom(BUFFER_SIZE)
+    thread1 = myThread(svrmsg,clntAddr)
+    thread1.start()# only one thread is running here, however mor
